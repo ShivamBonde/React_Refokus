9bba6c4aea045eedaaeb72806350c3c16737bab3
Shivam Bonde <shivambonde19@gmail.com>
2024-07-13 10:57:10 +0530

    navbar completed

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { IoIosReturnRight } from "react-icons/io";
+
+const Button = () => {
+  return (
+    <div className='min-w-40 px-4 py-[4px] rounded-full bg-zinc-100 flex items-center justify-between text-black'>
+        <span className='text-sm font-medium'>Get Started</span>
+        <IoIosReturnRight />
+    </div>
+  )
+}
+
+export default Button
