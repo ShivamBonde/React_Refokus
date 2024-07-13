8dc6e989fafec3d97440209144b71b63200a817c
Shivam Bonde <shivambonde19@gmail.com>
2024-07-13 14:44:27 +0530

    work component created

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import { IoIosReturnRight } from "react-icons/io";
 
 const Button = () => {
   return (
-    <div className='min-w-40 px-4 py-[4px] rounded-full bg-zinc-100 flex items-center justify-between text-black'>
+    <div className='min-w-40 px-5 py-[4px] rounded-full bg-zinc-100 flex items-center justify-between text-black'>
         <span className='text-sm font-medium'>Get Started</span>
         <IoIosReturnRight />
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,29 +3,34 @@ import Button from "./Button";
 
 function Navbar() {
   return (
-    <div className="max-w-screen-lg  flex items-center justify-between border-b-[1px] border-zinc-500 mx-auto py-5">
-      <div className="flex items-center"><img
-        src="https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/63349803431f1562dccf1802_refokus%20logo.svg"
-        alt=""
-      />
-      <div className="Links  ml-16 flex gap-12">
-        {["Home", "Work", "Culture", "", "News"].map((elem, index) =>
-          elem.length === 0 ? (
-            <span className="w-[2px] h-10 bg-zinc-600"></span>
-          ) : (
-            <a href="#" className="font-light text-sm flex items-center gap-1">
-              {index == 1 && (
-                <span
-                  style={{ boxShadow: "0 0 0.45em #00FF19" }}
-                  className="inline-block w-[5px] h-[5px] rounded-full bg-green-500"
-                ></span>
-              )}
-              {elem}
-            </a>
-          )
-        )}
-      </div></div>
-      <Button/>
+    <div className="max-w-screen-lg  flex items-center justify-between border-b-[1px] border-zinc-500 mx-auto py-4">
+      <div className="flex items-center">
+        <img
+          src="https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/63349803431f1562dccf1802_refokus%20logo.svg"
+          alt=""
+        />
+        <div className="Links  ml-16 flex gap-12">
+          {["Home", "Work", "Culture", "", "News"].map((elem, index) =>
+            elem.length === 0 ? (
+              <span key={index} className="w-[2px] h-10 bg-zinc-600"></span>
+            ) : (
+              <a
+                href="#" key={index}
+                className="font-light text-sm flex items-center gap-1"
+              >
+                {index == 1 && (
+                  <span key={index}
+                    style={{ boxShadow: "0 0 0.45em #00FF19" }}
+                    className="inline-block w-[5px] h-[5px] rounded-full bg-green-500"
+                  ></span>
+                )}
+                {elem}
+              </a>
+            )
+          )}
+        </div>
+      </div>
+      <Button />
     </div>
   );
 }
